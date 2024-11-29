e4e364a04cf4bff3b9d8074e4134752afc49633f
Cuga77 <bogatov2021@mail.ru>
2024-11-29 14:23:30 +0300

    delete all not my code

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <random>
 #include <iomanip>
 #include <fstream>
-#include "model/MotionIntegrator.h"
 #include "model/Visualize.h"
 
 std::vector<double> generateVelocityMeasurements(const std::vector<double>& times,
@@ -14,7 +13,7 @@ std::vector<double> generateVelocityMeasurements(const std::vector<double>& time
                                                 double noise_std) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::normal_distribution<double> noise(0.0, noise_std);
+    std::normal_distribution noise(0.0, noise_std);
 
     std::vector<double> velocities;
     velocities.reserve(times.size());
@@ -55,32 +54,13 @@ int main() {
         times, omega_true, A_true, B_true, noise_std
     );
 
-    MotionIntegrator integrator(dt, t_start, t_end, times, measurements);
-
-    //оценка
-    double omega_est = integrator.getOmega();
-    double A_est = integrator.getACoef();
-    double B_est = integrator.getBCoef();
 
     printParameters("Истинные параметры", omega_true, A_true, B_true);
-    printParameters("Оцененные параметры", omega_est, A_est, B_est);
-
-    std::vector<my_Vector> numerical = integrator.integrate();
-    std::vector<my_Vector> theoretical = integrator.getTheoretical();
 
     std::ofstream iterations_file("integration_results.txt");
     if (iterations_file.is_open()) {
         iterations_file << "Time\tTheta_num\tOmega_num\tTheta_theo\tOmega_theo\tMeasured_v\n";
 
-        for (size_t i = 0; i < numerical.size(); i++) {
-            double t = t_start + i * dt;
-            iterations_file << t << "\t"
-                          << numerical[i].get(0) << "\t"
-                          << numerical[i].get(1) << "\t"
-                          << theoretical[i].get(0) << "\t"
-                          << theoretical[i].get(1) << "\t"
-                          << measurements[i] << "\n";
-        }
         iterations_file.close();
     }
 
@@ -91,12 +71,6 @@ int main() {
     std::vector<my_Vector> theoretical_vis;
     std::vector<my_Vector> measurements_vis;
 
-    double t = t_start;
-    for (size_t i = 0; i < numerical.size(); ++i) {
-        numerical_vis.push_back(MotionIntegrator::getFullState(numerical[i], t));
-        theoretical_vis.push_back(MotionIntegrator::getFullState(theoretical[i], t));
-        t += dt;
-    }
 
     for (size_t i = 0; i < measurements.size(); ++i) {
         my_Vector measurement_state(3);
@@ -111,8 +85,6 @@ int main() {
     visualizer.addTrace(measurements_vis);
     visualizer.saveTraceToFile("motion_results.txt");
 
-    double error = integrator.getMeasurementError(times, measurements);
-    std::cout << "\nСреднеквадратичная ошибка: " << error << std::endl;
 
     return 0;
 }
